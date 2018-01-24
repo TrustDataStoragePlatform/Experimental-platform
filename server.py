406b62ec870751d6ac6d4501646c60041668a8c1
zyang22 <zyang22@qq.com>
2018-01-24 15:23:53 +0800

    server

---
+++ server.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 22 17:09:43 2018
+
+@author: yz
+"""
+
+import threading
+
+class Server:
+    def __init__(self):
+        self.weights = None    # what to fill in here
+        self.num_samples = 0
+        self.lock = threading.Lock()
+        
+    def update_weights(self, num_samples, weights):
+        with self.lock:
+            self.num_samples += num_samples
+            for sw, w in zip(self.weights, weights):
+                sw += num_samples * w
+                
+    def get_weights(self):
+        with self.lock:
+            if self.num_samples == 0:
+                return self.weights
+            
+            for w in self.weights:
+                w /= self.num_samples
+                
+            self.num_samples = 0
+            return self.weights
